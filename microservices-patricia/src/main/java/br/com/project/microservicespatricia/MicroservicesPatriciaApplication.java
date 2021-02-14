21276e40a749a472996b0bb2fed8dc7381e7e1e8
patriciasilvarj0989 <ppss1989a@gmail.com>
2021-02-13 22:28:31 -0300

    first commit

---
+++ microservices-patricia/src/main/java/br/com/project/microservicespatricia/MicroservicesPatriciaApplication.java
@@ -0,0 +1,13 @@
+package br.com.project.microservicespatricia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesPatriciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesPatriciaApplication.class, args);
+	}
+
+}
